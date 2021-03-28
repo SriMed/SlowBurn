3c0e8d188a4c1088a8febd72f411946f6e78fda1
Srilakshmi Medarametla <bawesomesri2@outlook.com>
2021-03-28 10:39:11 -0400

    workable dtrees

--- dtree2.py
+++ dtree2.py
@@ -8,29 +8,15 @@ from sklearn.ensemble import ExtraTreesRegressor
 from sklearn.ensemble import GradientBoostingRegressor
 import random
 
-def read_dataset(filename, feature_cols, cat_feature_cols):
+def read_dataset(filename, feature_cols):
     df = pd.read_csv(filename)
-    print(df.head())
-    print(df.dtypes)
+    df.fillna(df.mean(), inplace=True)
 
-    # for f in cat_feature_cols:
-    #     df[f] = df[f].astype('category')
-        # df[f] = df[f].cat.codes
-
-    replace_map = {'Gender': {'Male': 0, 'Female': 1},
-                   'Company Type': {'Service': 0, 'Product': 1},
-                   'WFH Setup Available': {'Yes': 0, 'No': 1}}
-
-    df_repl = df.copy()
-    df_repl.replace(replace_map, inplace=True)
-    print(df_repl.head)
-
-    df_repl.fillna(df_repl.mean(), inplace=True)
-
-    X = df_repl.loc[:, feature_cols]
+    X = df.loc[:, feature_cols]
     print(X.shape)
-    y = df_repl['Burn Rate']
+    y = df['Burn Rate']
     print(y.shape)
+
     return X,y
 
 def dtrees(X_fit, y_fit, X_eval, y_eval, features, dt_file):
@@ -54,9 +40,8 @@ def dtrees(X_fit, y_fit, X_eval, y_eval, features, dt_file):
     accuracy = extra_rf_dtree.score(X_eval,y_eval)
     dt_file.write(f'Extremely Randomized Dtrees: {accuracy}\n')
 
-
     #Gradient Boosting Trees
-    gb_tree = GradientBoostingRegressor(n_estimators=10, learning_rate=1.0, max_depth=1, random_state=0).fit(X_fit, y_fit)
+    gb_tree = GradientBoostingRegressor(n_estimators=50, learning_rate=1.0, max_depth=2, random_state=0).fit(X_fit, y_fit)
     accuracy = gb_tree.score(X_eval, y_eval)
     dt_file.write(f'Gradient Boosting Dtrees: {accuracy}')
 
@@ -65,19 +50,19 @@ def dtrees(X_fit, y_fit, X_eval, y_eval, features, dt_file):
 def runtime():
     parser = argparse.ArgumentParser(
         description='Fit & Score Dtree Regressors')
-    parser.add_argument('-f', '--features', nargs='+', default=['Gender', 'Company Type', 'WFH Setup Available', 'Designation', 'Resource Allocation', 'Mental Fatigue Score'],
+    parser.add_argument('-f', '--features', nargs='+', default=['Designation', 'Resource Allocation', 'Mental Fatigue Score'],
                         help='the features to include')
-    parser.add_argument('-cf', '--cat_features', nargs='+',
-                        default=['Gender', 'Company Type', 'WFH Setup Available'],
-                        help='the features to include')
-    parser.add_argument('-ofn', '--outputfilename', default="output1")
+    # parser.add_argument('-cf', '--cat_features', nargs='+',
+    #                     default=['Gender', 'Company Type', 'WFH Setup Available'],
+    #                     help='the features to include')
+    parser.add_argument('-ofn', '--outputfilename', default="output2")
 
     args = parser.parse_args()
 
     features = args.features
-    cat_features = args.cat_features
+    # cat_features = args.cat_features
 
-    X_fit, y_fit = read_dataset('train.csv', features, cat_features)
+    X_fit, y_fit = read_dataset('train.csv', features)
     X_fit, X_eval, y_fit, y_eval = train_test_split(X_fit, y_fit, test_size=0.30, random_state=2)
 
     dt_file = open(args.outputfilename + '.txt', 'w')
